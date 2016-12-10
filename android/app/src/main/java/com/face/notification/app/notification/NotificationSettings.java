597a0e258a4c85be142b8fd37cdc8a6082bfe452
Thomas Czogalik <thomcz@users.noreply.github.com>
2016-12-10 04:30:05 +0100

    Update NotificationSettings.java

--- android/app/src/main/java/com/face/notification/app/notification/NotificationSettings.java
+++ android/app/src/main/java/com/face/notification/app/notification/NotificationSettings.java
@@ -2,7 +2,7 @@ package com.face.notification.app.notification;
 
 public class NotificationSettings {
 
-    public static String SenderId = "340361911374";
-    public static String HubName = "FaceNotificationHub";
-    public static String HubListenConnectionString = "Endpoint=sb://facenotificationhub.servicebus.windows.net/;SharedAccessKeyName=DefaultListenSharedAccessSignature;SharedAccessKey=dchPPMZ6hOR/ihN7GbIwLqr8JH4zXaFL+xB5rL1X4DQ=";
+    public static String SenderId = "<sender id firebase>";
+    public static String HubName = "<azure notification hub>";
+    public static String HubListenConnectionString = "<DefaultListenSharedAccessSignature azure>";
 }
